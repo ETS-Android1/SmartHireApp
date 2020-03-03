4ee797d8da4bc3a69a015fbcd00a204b4632d3e2
yujune <teeyj-wp17@student.tarc.edu.my>
2020-03-03 13:21:36 +0800

    Trying to implement intent but fail

--- app/src/main/java/com/example/hire/MainActivity.java
+++ app/src/main/java/com/example/hire/MainActivity.java
@@ -2,13 +2,19 @@ package com.example.hire;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -48,6 +54,9 @@ public class MainActivity extends AppCompatActivity {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     String currentImagePath = null;
     Uri imageUri;
+    private static final int CAMERA_REQUEST_CODE = 200;
+    private static final int STORAGE_REQUEST_CODE = 400;
+    String camaraPermission[];
 
 
     @Override
@@ -61,14 +70,20 @@ public class MainActivity extends AppCompatActivity {
         imageViewResume = findViewById(R.id.imageViewResume);
         textViewExtractedText = findViewById(R.id.textViewExtractedText);
 
+        camaraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        //camaraPermission = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                if (!checkCameraPermission()) {
+                    requestCameraPermission();
+                } else {
+                    dispatchTakePictureIntent();
 
-                dispatchTakePictureIntent();
-
-                textViewExtractedText.setText("");
+                    textViewExtractedText.setText("");
+                }
 
             }
         });
@@ -89,13 +104,23 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void requestCameraPermission() {
+        ActivityCompat.requestPermissions(this, camaraPermission, CAMERA_REQUEST_CODE);
+    }
+
+    private boolean checkCameraPermission() {
+        boolean result = ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == (PackageManager.PERMISSION_GRANTED);
+        boolean result1 = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == (PackageManager.PERMISSION_GRANTED);
+        return result && result1;
+    }
+
     private void displayImage() {
         Intent intent = new Intent(this, DisplayImage.class);
         intent.putExtra("image_path", currentImagePath);
         startActivity(intent);
     }
 
-    private void dispatchTakePictureIntent() {
+    /*private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
 
@@ -108,12 +133,32 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (imageFile != null) {
+                //ContentValues values = new ContentValues();
+                //values.put(MediaStore.Images.Media.TITLE,"NewPic");
+                //values.put(MediaStore.Images.Media.DESCRIPTION,"Image to Text");
+                //imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,values);
                 imageUri = FileProvider.getUriForFile(this, "com.example.android.fileprovider", imageFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
 
         }
+    }*/
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+
+            ContentValues values = new ContentValues();
+            Toast.makeText(MainActivity.this, "hi:", Toast.LENGTH_SHORT).show();
+            values.put(MediaStore.Images.Media.TITLE, "NewPic");
+            values.put(MediaStore.Images.Media.DESCRIPTION, "Image to Text");
+            imageUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+
     }
 
 
@@ -171,13 +216,13 @@ public class MainActivity extends AppCompatActivity {
 
         TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
 
-        if(!recognizer.isOperational()){
-            Toast.makeText(MainActivity.this, "Error :" , Toast.LENGTH_SHORT).show();
-        }else{
+        if (!recognizer.isOperational()) {
+            Toast.makeText(MainActivity.this, "Error :", Toast.LENGTH_SHORT).show();
+        } else {
             Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
             SparseArray<TextBlock> items = recognizer.detect(frame);
             StringBuilder sb = new StringBuilder();
-            for(int i=0;i<items.size();i++){
+            for (int i = 0; i < items.size(); i++) {
                 TextBlock myItems = items.valueAt(i);
                 sb.append(myItems.getValue());
                 sb.append("\n");
@@ -217,6 +262,23 @@ public class MainActivity extends AppCompatActivity {
         return imageFile;
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(requestCode){
+            case CAMERA_REQUEST_CODE:
+                if(grantResults.length>0){
+                    boolean cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean writeStorageAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    Log.d("myTag","Camera:"+cameraAccepted+" Storage:"+writeStorageAccepted);
+                    if(cameraAccepted && writeStorageAccepted){
+                        dispatchTakePictureIntent();
+                    }else{
+                        Toast.makeText(MainActivity.this,"Permission Denied!" ,Toast.LENGTH_SHORT).show();
+                    }
+                }
+        }
     }
 
 }
