f77c49a9d65c799be18a7c65e78c9c49750ab722
yujune <teeyj-wp17@student.tarc.edu.my>
2020-03-16 16:04:46 +0800

    NER is still shit but i use regex to extract phone number and email. It works as expected but NER is still a problem, because it perform better when u give it a structured sentences.

--- app/src/main/java/com/example/hire/MainActivity.java
+++ app/src/main/java/com/example/hire/MainActivity.java
@@ -45,6 +45,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 //import edu.stanford.nlp.pipeline.*;
 
@@ -89,6 +91,20 @@ public class MainActivity extends AppCompatActivity {
                 //jsonParse();
                 //Submit("Tan Hao Yang lives in Seetapak");
                 //postData();
+
+                String str = "This is 1 test 011-10831511 and 0111083223 and the email is yujune00@yahoo.com";
+
+                Pattern pattern = Pattern.compile("\\d{3}-?\\d{7,8}");
+                Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+                Matcher matcher = pattern.matcher(str);
+                while (matcher.find()) {
+                    textViewExtractedText.append("Phone Number: "+matcher.group(0)+"\n");
+                }
+
+                Matcher emailMatcher = emailPattern.matcher(str);
+                if (emailMatcher.find()) {
+                    textViewExtractedText.append("Email: "+emailMatcher.group(0)+"\n");
+                }
             }
         });
 
@@ -172,7 +188,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Post Request For JSONObject
-    public void postData(String extractedText) {
+    public void postData(final String extractedText) {
         RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
         JSONObject object = new JSONObject();
         try {
@@ -196,6 +212,19 @@ public class MainActivity extends AppCompatActivity {
                                 String namedEntity = obj2.getString("ner");
                                 String namedEntityResult = obj2.getString("text");
                                 textViewExtractedText.append(namedEntity+" : "+namedEntityResult+"\n");
+                            }
+                            String str = extractedText;
+
+                            Pattern pattern = Pattern.compile("\\d{3}-?\\d{7,8}");
+                            Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+                            Matcher matcher = pattern.matcher(str);
+                            while (matcher.find()) {
+                                textViewExtractedText.append("Phone Number: "+matcher.group(0)+"\n");
+                            }
+
+                            Matcher emailMatcher = emailPattern.matcher(str);
+                            if (emailMatcher.find()) {
+                                textViewExtractedText.append("Email: "+emailMatcher.group(0)+"\n");
                             }
 
 
@@ -414,6 +443,7 @@ public class MainActivity extends AppCompatActivity {
             String extractedTextFromImage = sb.toString();
             //textViewExtractedText.setText(extractedTextFromImage);
             postData(extractedTextFromImage);
+
 
         }
 
