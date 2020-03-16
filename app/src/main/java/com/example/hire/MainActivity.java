bed56b1fadbd8b1484d6069cdeef486b970fc6f9
yujune <teeyj-wp17@student.tarc.edu.my>
2020-03-16 22:43:25 +0800

    NER is still shit but i use regex to extract phone number and email. It works as expected but NER is still a problem, because it perform better when u give it a structured sentences.

--- app/src/main/java/com/example/hire/MainActivity.java
+++ app/src/main/java/com/example/hire/MainActivity.java
@@ -91,20 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 //jsonParse();
                 //Submit("Tan Hao Yang lives in Seetapak");
                 //postData();
-
-                String str = "This is 1 test 011-10831511 and 0111083223 and the email is yujune00@yahoo.com";
-
-                Pattern pattern = Pattern.compile("\\d{3}-?\\d{7,8}");
-                Pattern emailPattern = Pattern.compile("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
-                Matcher matcher = pattern.matcher(str);
-                while (matcher.find()) {
-                    textViewExtractedText.append("Phone Number: "+matcher.group(0)+"\n");
-                }
-
-                Matcher emailMatcher = emailPattern.matcher(str);
-                if (emailMatcher.find()) {
-                    textViewExtractedText.append("Email: "+emailMatcher.group(0)+"\n");
-                }
+                extractText();
             }
         });
 
@@ -142,6 +129,31 @@ public class MainActivity extends AppCompatActivity {
                 //displayImage();
             }
         });
+    }
+
+    private void extractText() {
+
+        TextRecognizer recognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+
+        if (!recognizer.isOperational()) {
+            Toast.makeText(MainActivity.this, "Error :", Toast.LENGTH_SHORT).show();
+        } else {
+            Frame frame = new Frame.Builder().setBitmap(imageBitmap).build();
+            SparseArray<TextBlock> items = recognizer.detect(frame);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < items.size(); i++) {
+                TextBlock myItems = items.valueAt(i);
+                sb.append(myItems.getValue());
+                sb.append("\n");
+            }
+            /*Intent intent1 = new Intent(this,ExtractedText.class);
+            Bundle bundle = new Bundle();
+            bundle.putString("BundleText",sb.toString());
+            intent1.putExtra("EXTRACTED_TEXT",bundle);
+            startActivity(intent1);*/
+            String extractedTextFromImage = sb.toString();
+            textViewExtractedText.setText(extractedTextFromImage);
+        }
     }
 
     private void jsonParse() {
