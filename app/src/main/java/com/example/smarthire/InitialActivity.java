c80150e114af9097b13198f1a8c2d0221befe0ee
Pohcs123 <gencapoh@gmail.com>
2020-06-14 14:36:38 +0800

    6th commit

--- app/src/main/java/com/example/smarthire/InitialActivity.java
+++ app/src/main/java/com/example/smarthire/InitialActivity.java
@@ -2,7 +2,11 @@ package com.example.smarthire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class InitialActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class InitialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_initial);
+
+        SharedPreferences sharedPref = getSharedPreferences("PREF", Context.MODE_PRIVATE);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                if (sharedPref.getString("USER_ID","") != null && !sharedPref.getString("USER_ID","").equals("LOGGED_OUT")){
+                    Intent intent = new Intent(getApplicationContext(), BottomNavigationActivity.class);
+                    startActivity(intent);
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), Login.class);
+                    startActivity(intent);
+                }
+            }
+        }, 2500);   //5 seconds
+
+
+
     }
 }
